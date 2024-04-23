2a81b9b9d65c1d936671b90fdd0ca05b9b3be407
Asylm9 <167087374+Asylm9@users.noreply.github.com>
2024-04-23 11:37:54 +0200

    Update Ex.3 Seconds-Minutes-Hours.py

--- Projet 2  Convertisseurs/Ex.3 Seconds-Minutes-Hours.py
+++ Projet 2  Convertisseurs/Ex.3 Seconds-Minutes-Hours.py
@@ -14,22 +14,18 @@
 
 #Ex 3.2
 
-s=input("Secondes: ")
+s = input("Secondes: ")
+s = int(s)
 
-hs=3600
-h=int(s)//hs
+hs = 3600
+h = s // hs
 
-ms=60
+ms = 60
+m = (s%hs) // 60
 
-m=int(s)%hs
-
-s=hs%ms
-
-
-print("est égal à "+str(h)+" heures, "+str(m)+" minutes,"+str(s)+" secondes.")
+ss = (s%hs) % 60
 
 
-# +str(round(m))
 
-
+print("est égal à "+str(h)+" heures, "+str(m)+" minutes,"+str(ss)+" secondes.")
 
